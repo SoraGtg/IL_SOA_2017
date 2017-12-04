ab81d7bb4d3030d76e51428fe2dc7b40c60f719d
castanet <castanet@localhost>
2017-12-04 15:31:21 +0100

    Clarifié ce qu'il reste à faire

--- platform/src/main/java/controller/DatabaseOperator.java
+++ platform/src/main/java/controller/DatabaseOperator.java
@@ -7,24 +7,38 @@ import model.UserDoesNotExistException;
 
 public class DatabaseOperator {
 
+	// Credentials to connect to the database
 	private String dbURL = "jdbc:postgresql://baasu.db.elephantsql.com:5432/kyzksfba";
 	private String dbUsername = "kyzksfba";
 	private String dbPassword = "eOpz1rPDBSMo7fICNIu6Otj47Rc7rKdd";
+	
+	//Connection element
+	private Connection db ;
 
 	public DatabaseOperator() {
 		// Nothing to do ???
 	}
 	
-	public User getUser(String username) throws UserDoesNotExistException, SQLException, ClassNotFoundException  {
+	public void connectToDatabase() throws ClassNotFoundException, SQLException {
+		// TODO : renvoyer une erreur si on est déjà connecté
+        Class.forName("org.postgresql.Driver");
+    	//org.postgresql.Driver driver = new org.postgresql.Driver() ;
+        
+        this.db = DriverManager.getConnection(this.dbURL, this.dbUsername, this.dbPassword);
+		
+	}
+	
+	public void disconnectFromDatabase() {
+		
+	}
+	
+	public User getUser(String username) throws UserDoesNotExistException, SQLException  {
+		// TODO : renvoyer des erreurs si pas encore connecté
 		// Declaration of variables needed to create a User
 		int id;
 		int usertype;
-		
-        Class.forName("org.postgresql.Driver");
-    	//org.postgresql.Driver driver = new org.postgresql.Driver() ;
 
         // Declaration of variables needed  for connection to database
-        Connection db = DriverManager.getConnection(this.dbURL, this.dbUsername, this.dbPassword);
         Statement st = db.createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM users WHERE username = " + username);
         // Autre possibilité : PreparedStatement requete = db.prepareStatement("SELECT * FROM users WHERE username = ?");
